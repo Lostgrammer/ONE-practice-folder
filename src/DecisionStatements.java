34b0548f160c2e7e2991c05d1d09a6b513ea45de
Carlos Vega <carlos_cmvp@hotmail.com>
2024-04-05 13:53:48 -0600

    add extra if statement excersice

--- src/DecisionStatements.java
+++ src/DecisionStatements.java
@@ -2,8 +2,17 @@ public class DecisionStatements {
     public static void main(String[] args) {
         String nameMovie = "Bolt";
         int launchDate = 1999;
+        boolean availableMovie = true;
+        String packagePlanStatus = "normal";
+
         if (launchDate < 2022) {
-            System.out.println("La pelicula "+nameMovie+ " no es reciente");
+            System.out.println("La pelicula "+ nameMovie + " no es reciente");
+        }
+
+        if (availableMovie && packagePlanStatus == "plus"){
+            System.out.println("La pelicula "+ nameMovie + " esta disponible");
+        }else {
+            System.out.println("Su plan actual no cubre la pelicula "+nameMovie);
         }
     }
 }
