fb964466b712a962ee6640e9d685e420838354d8
Carlos Vega <carlos_cmvp@hotmail.com>
2024-04-05 13:41:28 -0600

    add new Class for decision statements

---
+++ src/DecisionStatements.java
@@ -0,0 +1,9 @@
+public class DecisionStatements {
+    public static void main(String[] args) {
+        String nameMovie = "Bolt";
+        int launchDate = 1999;
+        if (launchDate < 2022) {
+            System.out.println("La pelicula "+nameMovie+ " no es reciente");
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,26 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main (String[] args) {
-//        System.out.println ("You can always start anew");
-//        System.out.println ("Bienvenidos a la webapp para ver peliculas");
-//        String nameMovie = "Bolt";
-//        int launchDate = 1999;
-//        double rateMovie = (3.2 + 5 + 4.2) / 3;
-//        //casting
-//        int rateMovieInt = (int) rateMovie;
-//        System.out.println ("Nombre pelicula: " + nameMovie);
-//        System.out.println ("Fecha de lamzamiento: " + launchDate);
-//        System.out.println ("Valoracion: " + rateMovieInt);
-//        System.out.println ("""
-//                              SINOPSIS
-//                              Se trata de un perro con superpoderes
-//                              pero solo dentro de las camaras en los estudios de grabacion
-//                              ya que en la vida real el es un simple perro
-//                                """);
-//        /*
-//        * Este es un comentario
-//        * de muchas lineas
-//        * */
+        System.out.println ("You can always start anew");
+        System.out.println ("Bienvenidos a la webapp para ver peliculas");
+        String nameMovie = "Bolt";
+        int launchDate = 1999;
+        double rateMovie = (3.2 + 5 + 4.2) / 3;
+        //casting
+        int rateMovieInt = (int) rateMovie;
+        System.out.println ("Nombre pelicula: " + nameMovie);
+        System.out.println ("Fecha de lamzamiento: " + launchDate);
+        System.out.println ("Valoracion: " + rateMovieInt);
+        System.out.println ("""
+                              SINOPSIS
+                              Se trata de un perro con superpoderes
+                              pero solo dentro de las camaras en los estudios de grabacion
+                              ya que en la vida real el es un simple perro
+                                """);
+        /*
+        * Este es un comentario
+        * de muchas lineas
+        * */
 
         //cast practice
         Scanner input = new Scanner(System.in);
