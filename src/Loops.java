a6d04846cf40c2f9a2ea702d61426679a70c0150
Carlos Vega <carlos_cmvp@hotmail.com>
2024-04-08 10:23:36 -0600

    add loops class for practice

--- src/Loops.java
+++ src/Loops.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Loops {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int limitUser = 3;
+        double rating = 0;
+        double finalRating = 0;
+
+        for (int counter = 1; counter <= limitUser; counter++){
+            System.out.println("Calificacion usuario" + counter + ":");
+            double grade = input.nextDouble();
+            rating += grade;
+        }
+        finalRating = rating / limitUser;
+        System.out.println("La calificacion final es: " + finalRating + "/10");
+    }
 }
