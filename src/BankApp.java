5bc56526611887164c34e11e1a5b8f1dede0e1f5
Carlos Vega <carlos_cmvp@hotmail.com>
2024-04-08 16:50:57 -0600

    add BanckApp class and initial variables

---
+++ src/BankApp.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class BankApp {
+    /*
+     * objetivos de la app bancaria
+     * - tiene que tener 5 opciones:
+     * 1. consultar saldo
+     * 2. retirar dinero
+     *       si el retiro es mayor al saldo avisar al cliente
+     * 3. depositar dinero
+     * 4. salir
+     * variables
+     *   double:saldo = 1599.00;
+     *   String:nombre cliente = "Tony Stack";
+     *   int:opcionElegida = 0;
+     *   double:input dineroRetirar = 0, dineroDepositar=0;
+     *   double: newMoneyAmount;
+     * */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String username = "";
+        double initialBalance = 1599.00;
+        int chosenOption = 0;
+        double withdrawnMoney = 0;
+        double depositedMoney = 0;
+        double newBalance = 0;
+
+
+    }
+}
