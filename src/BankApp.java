e960b91e59b0a90447b02c651316613971af0a43
Carlos Vega <carlos_cmvp@hotmail.com>
2024-04-08 23:46:28 -0600

    add do while and if for choses

--- src/BankApp.java
+++ src/BankApp.java
@@ -14,7 +14,7 @@ public class BankApp {
      *   String:nombre cliente = "Tony Stack";
      *   int:opcionElegida = 0;
      *   double:input dineroRetirar = 0, dineroDepositar=0;
-     *   double: newMoneyAmount;
+     *   double: newBalance;
      * */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -26,6 +26,37 @@ public class BankApp {
         double depositedMoney = 0;
         double newBalance = 0;
 
+        //init app
+        username = "Tony Stark";
+        System.out.println("Bienvenido Sr(" + username + ")");
+        do {
+            System.out.println("Que operacion quiere realizar?\n" +
+                    "1. Consultar su saldo\n" +
+                    "2. Retirar\n" +
+                    "3. Depositar\n" +
+                    "9. Salir\n");
+            chosenOption = input.nextInt();
+            if (chosenOption == 1){
+                newBalance = initialBalance;
+                System.out.println("Su saldo actual es de $" + newBalance);
+            } else if (chosenOption == 2) {
+                System.out.println("Indique su monto a retirar:");
+                //if si el monto ingresado es superior
+//                if (){
+//
+//                }
+            } else if (chosenOption == 3) {
+                System.out.println("3");
+            }else if (chosenOption == 9) {
+                System.out.println("Que tenga buen dia!");
+            }else {
+                System.out.println("=========================\n" +
+                        "Seleecione una opcion valida\n" +
+                        "=========================");
+            }
+        } while (chosenOption != 9);
+
+        System.out.println();
 
     }
 }
