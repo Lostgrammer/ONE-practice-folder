626705031f46f142ee782a9dc093e243d76ab56d
Carlos Vega <carlos_cmvp@hotmail.com>
2024-04-09 16:33:47 -0600

    add getters and setters for totalRating and totalEvaluations

--- src/OOPScreenMatch/Movie.java
+++ src/OOPScreenMatch/Movie.java
@@ -1,18 +1,44 @@
 package OOPScreenMatch;
 
-public class Pelicula {
+
+public class Movie {
     //atributos
     String name;
     int launchYear;
-    String directorName;
-    String distributorName;
     int duration;
+    private double totalRating;
+    private int totalEvaluations;
 
     //metodos
     public void showData(){
         System.out.println("Nombre de pelicula: "+ name + "\n" +
                 "año de lanzamiento: " + launchYear + "\n" +
-                "Director: " + directorName + "\n" +
-                "duracion: " + duration  + "\n");
+                "duracion: " + duration);
+    }
+
+    public void rate(double rateValue){
+        System.out.println("Tu calificacion a esta pelicula: " + rateValue + "/10\n");
+        totalRating += rateValue;
+        totalEvaluations++;
+
+    }
+    public double calculateRatingMedia (){
+        return (totalRating) / totalEvaluations;
+    }
+
+    public int getTotalEvaluations() {
+        return totalEvaluations;
+    }
+
+    public void setTotalEvaluations(int totalEvaluations) {
+        this.totalEvaluations = totalEvaluations;
+    }
+
+    public double getTotalRating() {
+        return totalRating;
+    }
+
+    public void setTotalRating(double totalRating) {
+        this.totalRating = totalRating;
     }
 }
