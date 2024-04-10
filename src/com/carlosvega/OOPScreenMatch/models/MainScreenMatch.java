2e9ec3b3f6b07459cc53621ccc5bffa5a05409cb
Carlos Vega <carlos_cmvp@hotmail.com>
2024-04-09 23:31:09 -0600

    set private all Movie class atributes

--- src/com/carlosvega/OOPScreenMatch/models/MainScreenMatch.java
+++ src/com/carlosvega/OOPScreenMatch/models/MainScreenMatch.java
@@ -1,4 +1,4 @@
-package OOPScreenMatch;
+package com.carlosvega.OOPScreenMatch.models;
 
 
 
@@ -6,23 +6,13 @@ public class MainScreenMatch {
     public static void main(String[] args) {
 
         Movie peli1 = new Movie();
-        peli1.name = "Planeta de los Simios";
-        peli1.launchYear = 2011;
-        peli1.duration = 120;
-
+        peli1.setName("Planeta de los Simios");
+        peli1.setLaunchYear(2011);
+        peli1.setDuration(120);
         peli1.showData();
         peli1.rate(5.7);
         peli1.rate(10);
         System.out.println("La media de la pelicula es: " + peli1.calculateRatingMedia() + " /10");
         System.out.println("getter de peli1:"+ peli1.getTotalRating());
-
-        Movie peli2 = new Movie();
-        peli2.name = "Amelie";
-        peli2.launchYear = 2002;
-        peli2.duration = 90;
-
-
-//        peli2.showData();
-//        peli2.rate(9);
     }
 }
--- src/com/carlosvega/OOPScreenMatch/models/Movie.java
+++ src/com/carlosvega/OOPScreenMatch/models/Movie.java
@@ -1,13 +1,30 @@
-package OOPScreenMatch;
+package com.carlosvega.OOPScreenMatch.models;
 
 
 public class Movie {
     //atributos
-    String name;
-    int launchYear;
-    int duration;
+    private String name;
+    private int launchYear;
+    private int duration;
     private double totalRating;
     private int totalEvaluations;
+
+    //getters y setters
+    public double getTotalRating() {
+        return totalRating;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public void setLaunchYear(int launchYear) {
+        this.launchYear = launchYear;
+    }
 
     //metodos
     public void showData(){
@@ -15,7 +32,6 @@ public class Movie {
                 "año de lanzamiento: " + launchYear + "\n" +
                 "duracion: " + duration);
     }
-
     public void rate(double rateValue){
         System.out.println("Tu calificacion a esta pelicula: " + rateValue + "/10\n");
         totalRating += rateValue;
@@ -26,19 +42,4 @@ public class Movie {
         return (totalRating) / totalEvaluations;
     }
 
-    public int getTotalEvaluations() {
-        return totalEvaluations;
-    }
-
-    public void setTotalEvaluations(int totalEvaluations) {
-        this.totalEvaluations = totalEvaluations;
-    }
-
-    public double getTotalRating() {
-        return totalRating;
-    }
-
-    public void setTotalRating(double totalRating) {
-        this.totalRating = totalRating;
-    }
 }
