ca028c187cbb3433ba2b821868689a2a7ff4045d
Carlos Vega <carlos_cmvp@hotmail.com>
2024-04-10 16:47:02 -0600

    create subclass Series

---
+++ src/com/carlosvega/OOPScreenMatch/models/Series.java
@@ -0,0 +1,9 @@
+package com.carlosvega.OOPScreenMatch.models;
+
+public class Series extends Movie{
+    private int QuantitySeasons;
+    public Series(String name){
+        super(name);
+
+    }
+}
