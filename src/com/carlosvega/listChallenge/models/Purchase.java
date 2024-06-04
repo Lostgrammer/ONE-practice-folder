f4c754e90867ff9e08434301d66b84af52444ee2
Carlos Vega <carlos_cmvp@hotmail.com>
2024-06-04 15:01:41 -0600

    add purchase class

--- src/com/carlosvega/listChallenge/models/Purchase.java
+++ src/com/carlosvega/listChallenge/models/Purchase.java
@@ -3,6 +3,18 @@ package com.carlosvega.listChallenge.models;
 public class Purchase {
     private String description;
     private double value;
-    private String  ticket;
 
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getValue() {
+        return value;
+    }
+    public void setValue(double value) {
+        this.value = value;
+    }
 }
