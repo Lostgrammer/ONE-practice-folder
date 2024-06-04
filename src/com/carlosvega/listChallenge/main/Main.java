b62a976538566f4876d34d4e671112ac129d8532
Carlos Vega <carlos_cmvp@hotmail.com>
2024-06-04 14:53:07 -0600

    define classes and main class

--- src/com/carlosvega/listChallenge/main/Main.java
+++ src/com/carlosvega/listChallenge/main/Main.java
@@ -2,6 +2,7 @@ package com.carlosvega.listChallenge.main;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Bienvenido a la tienda");
+        System.out.println("Ingrese el monto de su tarjeta por seguridad");
     }
 }
