ca4560dd48f13e92f9fad2e452df48e1d91f8f7f
Carlos Vega <carlos_cmvp@hotmail.com>
2024-06-04 15:58:06 -0600

    insert variables and list of Purchase objects

--- src/com/carlosvega/listChallenge/main/Main.java
+++ src/com/carlosvega/listChallenge/main/Main.java
@@ -1,8 +1,39 @@
 package com.carlosvega.listChallenge.main;
 
+import com.carlosvega.listChallenge.models.Card;
+import com.carlosvega.listChallenge.models.Purchase;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        //variables
+        Scanner input  = new Scanner(System.in);
+        Card myCard = new Card();
+        double accumulatedValue = 0;
+        String temporalDescription = "";
+        double temporalValue = 0;
+        List<Purchase> purchaseList = new ArrayList<>();
+
+        //print welcome
         System.out.println("Bienvenido a la tienda");
         System.out.println("Ingrese el monto de su tarjeta por seguridad");
+        myCard.setLimit(input.nextDouble());
+        while (myCard.getLimit() > accumulatedValue){
+            System.out.println("Ingrese la descripcion del producto:");
+            temporalDescription = input.nextLine();
+            System.out.println("Ingrese el precio del producto");
+            temporalValue = input.nextDouble();
+            accumulatedValue +=temporalValue;
+            purchaseList.add(new Purchase(temporalDescription, temporalValue));
+            //if si desea seguir comprando
+            //else
+            //break;
+        }
+
+
+
     }
 }
--- src/com/carlosvega/listChallenge/models/Purchase.java
+++ src/com/carlosvega/listChallenge/models/Purchase.java
@@ -4,6 +4,10 @@ public class Purchase {
     private String description;
     private double value;
 
+    public Purchase(String description,double value){
+        this.description = description;
+        this.value = value;
+    }
     public String getDescription() {
         return description;
     }
