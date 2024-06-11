4ab5dbe19bda7f6dc4c6c355d909817047fec957
Carlos Vega <carlos_cmvp@hotmail.com>
2024-06-11 00:04:37 -0600

    add list Purchase, fix getters and setters

--- src/com/carlosvega/listChallenge/models/Card.java
+++ src/com/carlosvega/listChallenge/models/Card.java
@@ -1,12 +1,29 @@
 package com.carlosvega.listChallenge.models;
 
-public class Card {
-    private double balance;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Card {
+    private double balance;//saldo de tarjeta
+    private List <Purchase> purchaseList = new ArrayList<>();
+
+    //constructor
+    public Card(double balance) {
+        this.balance = balance;
+    } //first value of card balance
+
+    //getters and setters
     public double getBalance() {
         return balance;
     }
-    public void setBalance(double balance) {
-        this.balance = balance;
+    public void setBalance(double productValue) {
+        this.balance = this.balance - productValue;
+        System.out.println(balance);
+    }
+
+    //metodos
+    public List<Purchase> addPurchase(Purchase purchase){
+        purchaseList.add(purchase);
+        return purchaseList;
     }
 }
