66ffdea04feed26bb2ad80e19de1fb7c2fca6d87
Carlos Vega <carlos_cmvp@hotmail.com>
2024-06-17 13:35:53 -0600

    fix value of runTime to runtime

--- src/com/carlosvega/OOPScreenMatch/models/TitleOmdb.java
+++ src/com/carlosvega/OOPScreenMatch/models/TitleOmdb.java
@@ -1,5 +1,5 @@
 package com.carlosvega.OOPScreenMatch.models;
 
-public record TitleOmdb(String title, String year, String runTime) {//todos deben ser String porque recibe json que todos sus valores son String
+public record TitleOmdb(String title, String year, String runtime) {//todos deben ser String porque recibe json que todos sus valores son String
 
 }
